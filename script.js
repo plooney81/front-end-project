f9cbb31b82cfb4db7907621b2f73d20832e3f879
Pete Looney <petelooney81@gmail.com>
2020-09-24 16:31:20 -0500

    jquery

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 // We need to find a way to get the users address from the search bar
 // we also need to find a way to get the radius the user would like to see results for
 // but for now lets go ahead and hard code in the lat and long from the Cannon here in Houston
-let addressLat;
-let addressLong;
-let restaurantObject = {};
-let regex = new RegExp('(?:dog|pet|animal)');
-let dogFriendlyRestaurants = {};
+
 
 const googleApiKey = 'AIzaSyCrK3yusa4V5Evj1A2cwdxkb_iUR-WLCVk'; // Key for the multiple google apis we will be pulling from.
-const userAddress = 'Houston'; //This will eventually change.
-const searchRadius = 5000; // this needs to be in meters for the Google places api, we can convert from the user giving in miles.
-const urlEncodedUserAddress = encodeURIComponent(userAddress); // encodes the users address into an URI by replacing each instance of certain characters with %
+
 
 const googleGeocode = 'https://maps.googleapis.com/maps/api/geocode/json?'; // our geocode api starting URL, we will add onto it in the axios request
-const googlePlaces = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'; // our google places api starting URL, we add onto it in the second axios request
-const googlePlacesDetails = 'https://maps.googleapis.com/maps/api/place/details/json?';
-const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 //THIS BEGINS KATE'S CODE AND ENDS PETE'S CODE
 function renderRestaurants(friendlyRs) {
@@ -65,8 +56,6 @@ function noList(restaurantName) {
     localStorage.setItem('noList', noListJSON);
 
 }
-
-
 //THIS ENDS KATE'S CODE AND STARTS PETE'S CODE
 
 // Pete added on here
@@ -86,7 +75,7 @@ async function returnFourSquarePicture(restName, lat, long, searchDistance){
                     return '#';
                 }else{
                     if(secondResponse.data.response.photos.items[0]){
-                        console.log(`${secondResponse.data.response.photos.items[0].prefix}150x200${secondResponse.data.response.photos.items[0].suffix}`)
+                        // console.log(`${secondResponse.data.response.photos.items[0].prefix}150x200${secondResponse.data.response.photos.items[0].suffix}`)
                         return `${secondResponse.data.response.photos.items[0].prefix}150x200${secondResponse.data.response.photos.items[0].suffix}`
                     }else{
                         return '#';
@@ -97,83 +86,115 @@ async function returnFourSquarePicture(restName, lat, long, searchDistance){
 }
 // End of Petes addition
 
-axios.get(`${googleGeocode}address=${urlEncodedUserAddress}&key=${googleApiKey}`)
-    .then((response)=>{
-        console.log(response.data);
-        addressLat = response.data.results[0].geometry.location.lat;
-        addressLong = response.data.results[0].geometry.location.lng;
-        let loc = new google.maps.LatLng(addressLat, addressLong);
-        let map = new google.maps.Map(document.createElement('div'), {
-            center: loc,
-            zoom: 15
-            });
-        
-        let service = new google.maps.places.PlacesService(map);
-        
-        
-        let request = {
-            location: loc,
-            radius: `${searchRadius}`,
-            query: 'restaurant'
-        };
-        service.textSearch(request, (results, status)=>{
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                restaurantObject = results;
-                // console.log(restaurantObject);
-                const restArray = restaurantObject.map((currentRestaurant)=>{
-                    return currentRestaurant.place_id; //only returns the place_id from the restaurant data, when then loop over the ID's to get the details api information.
-                })
-                // console.log(restArray);
-                const placeDetailsArray = restArray.map((currentId)=>{
-                    let request2 = {
-                        placeId: currentId,
-                        fields: ['name', 'rating', 'geometry', 'reviews']
-                    };
-                    service.getDetails(request2, (place, status)=>{
-                        if (status == google.maps.places.PlacesServiceStatus.OK) {
-                            // console.log(place);
-                            const reviewsArray = place.reviews;
-                            
-                            reviewsArray.forEach((currentReview)=>{
-                                // console.log(regex.test(currentReview.text));
-                                if(regex.test(currentReview.text)){//So It will basically loop over the reviews array and hopefully will find the reviews with dog/pet/animal friendly in them...fingers crossed.
-                                    if (dogFriendlyRestaurants[place.name]){
-                                        dogFriendlyRestaurants[place.name].frequency += 1;
-                                    }else{
-                                        // Pete - added in a call for each place to get a FourSquare picture
-                                        let imgUrl = returnFourSquarePicture(place.name, addressLat, addressLong, searchRadius);
-                                        console.log(imgUrl);
-                                        dogFriendlyRestaurants[place.name] = {
-                                            'restaurantName' : place.name, 
-                                            'frequency': 1, 
-                                            'rating': place.rating, 
-                                            'reviews': place.reviews, 
-                                            'pic' : imgUrl
-                                        };
-                                    }
-                                }
-                            })
+// PETE ADDED BELOW
+// Jquery code below
+$(document).ready(()=>{
+    let addressLat;
+    let addressLong;
+    let restaurantObject = {};
+    let regex = new RegExp('(?:dog|pet|animal)');
+    let dogFriendlyRestaurants = {};
+    let userAddress = 'Chicago'; //This will eventually change.
+    let searchRadius = 5000; // this needs to be in meters for the Google places api, we can convert from the user giving in miles.
+    
+    const $search = $('#search-bar');
+    const $searchButton = $('#search-Btn');
+    const $dropDown = $('#searchRadius-dropdown')
+                                
+    $searchButton.click(()=>{
+        // grabs the value from the search radius dropdown 
+        if($dropDown.val() !== ''){
+            searchRadius = $dropDown.val();
+        }
 
-                            console.log(dogFriendlyRestaurants);
-                            //KATE HAS ADDED HERE
-                            let dogFriendly = [];
+        // grabs the value from the input text box.
+        if($search.val()){
+            userAddress = $search.val();
+            // console.log(userAddress);
+        }
 
-                            Object.keys(dogFriendlyRestaurants).forEach((key) => {
-                                dogFriendly.push(dogFriendlyRestaurants[key]);
-                            });
-
-                                console.log(dogFriendly)
-                                const starthere = document.querySelector('#starthere');
-                                starthere.innerHTML = renderRestaurants(dogFriendly);
-                            
-                        }
+        let urlEncodedUserAddress = encodeURIComponent(userAddress); // encodes the users address into an URI by replacing each instance of certain characters with %
+        axios.get(`${googleGeocode}address=${urlEncodedUserAddress}&key=${googleApiKey}`)
+            .then((response)=>{
+                console.log(response.data);
+                addressLat = response.data.results[0].geometry.location.lat;
+                addressLong = response.data.results[0].geometry.location.lng;
+                console.log(addressLat, addressLong);
+                let loc = new google.maps.LatLng(addressLat, addressLong);
+                let map = new google.maps.Map(document.createElement('div'), {
+                    center: loc,
+                    zoom: 15
                     });
-                    
-                })
-
-            }
-        });
+                
+                let service = new google.maps.places.PlacesService(map);
+                
+                
+                let request = {
+                    location: loc,
+                    radius: `${searchRadius}`,
+                    query: 'restaurant'
+                };
+                service.textSearch(request, (results, status)=>{
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        restaurantObject = results;
+                        console.log(restaurantObject);
+                        const restArray = restaurantObject.map((currentRestaurant)=>{
+                            return currentRestaurant.place_id; //only returns the place_id from the restaurant data, when then loop over the ID's to get the details api information.
+                        })
+                        // console.log(restArray);
+                        const placeDetailsArray = restArray.map((currentId)=>{
+                            let request2 = {
+                                placeId: currentId,
+                                fields: ['name', 'rating', 'geometry', 'reviews']
+                            };
+                            service.getDetails(request2, (place, status)=>{
+                                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                                    // console.log(place);
+                                    const reviewsArray = place.reviews;
+                                    
+                                    reviewsArray.forEach((currentReview)=>{
+                                        // console.log(regex.test(currentReview.text));
+                                        if(regex.test(currentReview.text)){//So It will basically loop over the reviews array and hopefully will find the reviews with dog/pet/animal friendly in them...fingers crossed.
+                                            if (dogFriendlyRestaurants[place.name]){
+                                                dogFriendlyRestaurants[place.name].frequency += 1;
+                                            }else{
+                                                // Pete - added in a call for each place to get a FourSquare picture
+                                                let imgUrl = returnFourSquarePicture(place.name, addressLat, addressLong, searchRadius);
+                                                // console.log(imgUrl);
+                                                dogFriendlyRestaurants[place.name] = {
+                                                    'restaurantName' : place.name, 
+                                                    'frequency': 1, 
+                                                    'rating': place.rating, 
+                                                    'reviews': place.reviews, 
+                                                    'pic' : imgUrl
+                                                };
+                                            }
+                                        }
+                                    })
         
-    });
+                                    // console.log(dogFriendlyRestaurants);
+                                    //KATE HAS ADDED HERE
+                                    let dogFriendly = [];
+        
+                                    Object.keys(dogFriendlyRestaurants).forEach((key) => {
+                                        dogFriendly.push(dogFriendlyRestaurants[key]);
+                                    });
+                                        // console.log(dogFriendly)
+                                        const starthere = document.querySelector('#starthere');
+                                        starthere.innerHTML = renderRestaurants(dogFriendly);
+                                    
+                                }
+                            });
+                            
+                        })
+        
+                    }
+                });
+                
+            });
+    })
+
+})
+
 
 
